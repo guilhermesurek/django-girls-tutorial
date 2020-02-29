5922ba10eeea1b0f2ea8160ecc02d1123fa8bf6d
guilhermesurek <guilhermesurek@gmail.com>
2020-02-29 11:09:57 -0300

    app aula4

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from aula4.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls'))
+    path('', include('blog.urls')),
+    path('aula4', index)
 ]

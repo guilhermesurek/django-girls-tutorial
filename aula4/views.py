8715a9a702adef1843e9cfa9fb27cbb80d1d7344
guilhermesurek <guilhermesurek@gmail.com>
2020-02-29 13:06:54 -0300

    app aula4

--- aula4/views.py
+++ aula4/views.py
@@ -3,4 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'aula4/aula4.html')
+    context = {
+        "alunos": [
+            "joao",
+            "maria",
+            "pedro",
+            "jose",
+        ]
+    }
+    return render(request, 'aula4/index.html', context=context)
